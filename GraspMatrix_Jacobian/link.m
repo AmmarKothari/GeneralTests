982c33c731e575cbaf7741a2ccbfd817a7a31220
Ammar <amarsbars@gmail.com>
2018-01-30 20:00:34 -0800

    minor changes

--- GraspMatrix_Jacobian/link.m
+++ GraspMatrix_Jacobian/link.m
@@ -1,13 +1,13 @@
 classdef link
     properties
         length
-        pose
+        pose_world
     end
 
     methods
-        function obj = link(length)
+        function obj = link(length, pose)
             obj.length = length;
-            obj.pose = pose;
+            obj.pose_world = pose;
         end
 
     end
