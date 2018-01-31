27e0c4f06e0731e8f48fd3db3dbc3018b34b8913
Ammar <amarsbars@gmail.com>
2018-01-30 18:26:58 -0800

    grasp matrix and jacobian practice

---
+++ GraspMatrix_Jacobian/draw_link.m
@@ -0,0 +1,17 @@
+classdef draw_link
+    properties
+        link
+    end
+
+    methods
+        function obj = draw_link(link)
+            obj.link = link;
+            obj.figure = figure()
+            obj.ax = 
+        end
+        
+        
+    end
+
+
+end
---
+++ GraspMatrix_Jacobian/link.m
@@ -0,0 +1,16 @@
+classdef link
+    properties
+        length
+        pose
+    end
+
+    methods
+        function obj = link(length)
+            obj.length = length;
+            obj.pose = pose;
+        end
+
+    end
+
+
+end
