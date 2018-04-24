57087ef16591191b3ae5915d2f876fab860cb6fd
Ammar <amarsbars@gmail.com>
2018-04-23 20:10:44 -0700

    importance sampling still not working right!

--- CrossEntropyMethod/CEM.py
+++ CrossEntropyMethod/CEM.py
@@ -15,6 +15,9 @@ def trueVal1D(v):
 	norm_samples = (v - mu_g)/std_g
 	return -abs(scipy.stats.norm.cdf(norm_samples) - 0.5)
 
+def pickBest(vals, keep):
+	return samples[np.argsort(vals)[-keep:]]
+
 
 # def CEM(N, m):
 
@@ -29,7 +32,7 @@ possible_range = (0,10)
 mu_s = sum(possible_range)/2
 std_s = 1
 iterations = 100
-samples_iter = 50
+samples_iter = 10
 keep_samples = 5
 
 colors = itertools.cycle(['g', 'b', 'r', 'y'])
@@ -39,11 +42,29 @@ for i in range(iterations):
 	samples = np.random.normal(mu_s, std_s, samples_iter)
 	sample_vals = trueVal1D(samples)
 	plt.hist(samples, color=next(colors))
-	best_samples = samples[np.argsort(sample_vals)[-keep_samples:]]
-	mu_s = np.mean(best_samples)
-	std_s = np.std(best_samples)
+	# do importance sampling
+	samples_scaled = (sample_vals-np.min(sample_vals))/(np.max(sample_vals)-np.min(sample_vals))
+	samples_norm = samples_scaled/np.sum(samples_scaled)
+	samples_norm_sum = np.cumsum(samples_norm)
+	# pdb.set_trace()
+	keepers = []
+	for s in np.random.rand(keep_samples):
+		idx = np.abs(samples_norm_sum - s).argmin()
+		if s > samples_norm_sum[idx]:
+			idx += 1
+		try:
+			keepers.append(samples[idx])
+		except:
+			pdb.set_trace()
+	# best_samples = pickBest(samples, keep_samples)
+	# mu_s = np.mean(best_samples)
+	# std_s = np.std(best_samples)
+
+	mu_s = np.mean(keepers)
+	std_s = np.std(keepers)
+
 	print('New Estimate: %s +/- %s' %(mu_s, std_s))
-	if std_s < 0.1:
-		std_s = 0.1
+	# if std_s < 0.1:
+	# 	std_s = 0.1
 plt.show()
 
