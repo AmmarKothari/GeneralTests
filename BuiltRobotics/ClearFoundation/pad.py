31ca83b93ed29134637fdd3e91298098fb6061dd
Ammar Kothari <ammar.kothari@gmail.com>
2019-02-15 08:15:01 -0800

    optimization is working, need to tweak cost function

--- BuiltRobotics/ClearFoundation/pad.py
+++ BuiltRobotics/ClearFoundation/pad.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pdb
 import shapely.geometry as sg
+import matplotlib.pyplot as plt
 
+# What is a better way to do this?
 class Pad(object):
 	def __init__(self, back_left=None, back_right=None, front_left=None, front_right=None, length=None, width=None, theta=None):
 		self.bl = back_left
@@ -11,7 +13,7 @@ class Pad(object):
 		self.l = length
 		self.w = width
 		self.theta = theta
-		if self.bl and self.br and self.l:
+		if self.bl is not None and self.br is not None and self.l is not None:
 			self.bl = np.array(self.bl)
 			self.br = np.array(self.br)
 			self.l = np.array(self.l)
@@ -19,14 +21,14 @@ class Pad(object):
 			self.theta = np.arctan2(self.br[1] - self.bl[1], self.br[0] - self.bl[0]) - np.pi/2
 			self.fl = self.bl + self.rotation(self.theta) * self.l
 			self.fr = self.br + self.rotation(self.theta) * self.l
-
-		# elif self.bl and self.fl and self.l and self.w:
-		# 	self.theta = np.arctan2(self.bl)
-
-
-		# if self.bl and self.br and self.l:
-		# 	self.w = np.linalg.norm(np.array(back_left) - np.array(back_right))
-			# self.theta = np.arctan2(back_left[1] - back_right[1], back_left[0] - back_right[0])
+		elif self.bl is not None and self.fl is not None  and self.w:
+			self.bl = np.array(self.bl)
+			self.fl = np.array(self.fl)
+			self.w = np.array(self.w)
+			self.l = np.linalg.norm(self.bl - self.fl)
+			self.theta = np.arctan2(self.fl[1] - self.bl[1], self.fl[0] - self.bl[0])  - np.pi/2
+			self.br = self.bl + self.rotation(self.theta) * self.w
+			self.fr = self.fl + self.rotation(self.theta) * self.w
 
 	@property
 	def back_center(self):
@@ -43,6 +45,11 @@ class Pad(object):
 	def rotation(self, theta):
 		rot = np.array([np.cos(theta), np.sin(theta)])
 		return rot
+
+	def plot(self):
+		x,y = self.poly.exterior.xy
+		plt.plot(x,y, 'rx-')
+		plt.show()
 
 
 
