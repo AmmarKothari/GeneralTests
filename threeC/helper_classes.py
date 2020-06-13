b6c78f44f4f99046d2c402db5e4f64bc7c8e14e7
ammarkothari <ammarkothari@gmail.com>
2020-06-13 14:46:36 -0700

    update to the max value class

--- threeC/helper_classes.py
+++ threeC/helper_classes.py
@@ -6,10 +6,12 @@ class MaxValueTracker:
     def reset_current_value(self):
         self.current_value = 0.0
 
-    def update_current_value(self, val):
+    def add_current_value(self, val):
         self.current_value += val
 
-    def update_max_value(self, current_val=None):
+    def update_max_value(self, current_val=None, reset=False):
         if current_val is None:
             current_val = self.current_value
         self.max_value = max(self.max_value, current_val)
+        if reset:
+            self.reset_current_value()
