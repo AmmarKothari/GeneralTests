dbd119a7f17d936d45bbe7a2ca8cb3b5fd704fef
ammarkothari <ammarkothari@gmail.com>
2020-11-11 21:20:38 -0800

    some typing.  request helper is not done.

--- threeC/account_info.py
+++ threeC/account_info.py
@@ -1,52 +1,59 @@
 import functools
 
-from request_helper import _check_if_request_successful, _check_with_retry
+import py3cw  # type: ignore
+from typing import Any, Dict, List
+
+import request_helper
 
 
-# TODO: Should this take an account id in the constructor so each object is a single account?
 class AccountInfo:
-    def __init__(self, cw, real=True):
+    def __init__(self, cw: py3cw.Py3CW, real: bool = True) -> None:
         self.cw = cw
+        mode: str
         if real:
             mode = 'real'
         else:
             mode = 'paper'
         success, accounts = self.cw.request(entity='users', action='change_mode', payload={'mode': mode})
-        _check_if_request_successful(success)
+        request_helper.check_if_request_successful(success)
 
-    @property
+    @property  # type: ignore
     @functools.lru_cache()
-    def accounts(self):
+    def accounts(self) -> request_helper.Py3cw_request_info_list:
         success, accounts = self.cw.request(entity='accounts', action='')
-        _check_if_request_successful(success)
+        request_helper.check_if_request_successful(success)
         return accounts
 
     @functools.lru_cache()
-    def account_table_data(self, account_id):
+    def account_table_data(self, account_id: int) -> request_helper.Py3cw_request_info:
         request_func = functools.partial(self.cw.request, entity='accounts', action='account_table_data', action_id=str(account_id))
-        table_data = _check_with_retry(request_func)
+        table_data = request_helper.check_with_retry(request_func)
         return table_data
 
     @property
-    def account_ids(self):
+    def account_ids(self) -> Dict[str, int]:
         account_ids = {}
         for account in self.accounts:
             account_ids[account['exchange_name']] = account['id']
         return account_ids
 
-    def get_account_balance(self, account_id):
+    def get_account_from_id(self, account_id: int) -> request_helper.Py3cw_request_info:
         for account in self.accounts:
             if account['id'] == account_id:
-                return float(account['btc_amount'])
+                return account
+        raise Exception(f'No account with id: {account_id}')
 
-    def get_account_profit(self, account_id):
-        for account in self.accounts:
-            if account['id'] == account_id:
-                return float(account['day_profit_btc'])
+    def get_account_balance(self, account_id: int) -> float:
+        account = self.get_account_from_id(account_id)
+        return float(account['btc_amount'])
+
+    def get_account_profit(self, account_id: int) -> float:
+        account = self.get_account_from_id(account_id)
+        return float(account['day_profit_btc'])
 
     def get_coin_in_account(self, coin, account_id):
         request_func = functools.partial(self.cw.request, entity='accounts', action='pie_chart_data', action_id=str(account_id))
-        pairs_info = _check_with_retry(request_func)
+        pairs_info = request_helper.check_with_retry(request_func)
         for pair in pairs_info:
             if pair['code'] == coin:
                 return pair['amount']
--- threeC/request_helper.py
+++ threeC/request_helper.py
@@ -1,3 +1,10 @@
+from typing import Any, Callable, Dict, Optional, List, Union, Tuple
+
+Py3cw_request_info = Optional[Dict[str, Any]]
+Py3cw_request_info_list = List[Optional[Dict[str, Any]]]
+Py3cw_request_success = Optional[Dict[str, Any]]
+Py3cw_request = Tuple[Py3cw_request_info, Union[Py3cw_request_success, Py3cw_request_info_list]]
+
 MAX_FAILURES = 3
 
 
@@ -5,20 +12,22 @@ class RequestError(Exception):
     pass
 
 
-def _check_if_request_successful(success):
+def check_if_request_successful(success: Py3cw_request_success) -> None:
     if success:
         raise RequestError('Failed to get data: {}'.format(success))
+    return None
 
 
-def _check_with_retry(request_func):
-    failure_counter = 0
+def check_with_retry(request_func: Callable[[], Py3cw_request]) -> Union[Py3cw_request_info, Py3cw_request_info_list]:
+    failure_counter: int = 0
     while failure_counter < MAX_FAILURES:
         success, info = request_func()
         try:
-            _check_if_request_successful(success)
+            check_if_request_successful(success)
             # Break if the request was successful
             return info
         except RequestError:
             failure_counter += 1
             print(f"Failed to get data {failure_counter} times.  Retrying.")
+    return None
 
